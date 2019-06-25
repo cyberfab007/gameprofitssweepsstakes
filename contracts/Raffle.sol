3a8884e7e8fcde1f9b3a0725a5e8cdb143a9c914
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-06-25 20:35:11 +0300

    some deposit() logic

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 import "./Owned.sol";
+import "./Token.sol";
 
 contract Raffle is Owned {
 
@@ -11,6 +12,9 @@ contract Raffle is Owned {
     uint32    public execTimestamp;
     uint32    public execDelay;
     string    public sponsoredBy;
+
+    mapping (address => uint256[]) public bets;
+    mapping (address => MyAdvancedToken) prizeTokensInstances;
 
     constructor(
           string    memory paramName,
@@ -31,8 +35,26 @@ contract Raffle is Owned {
         setSponsoredBy(paramSponsoredBy);
     }
 
-    function deposit() public {
+     /**
+     * Called by players when they want to deposit tickets. The winner will be identified 
+     * by the account belonging to the address of the sender depositing the tickets.
+     *
+     * @param prizeToken the prize token which the player wants to deposit to make a bet
+     * @param ticketNumbers the ticket numbers on which the player deposits his prize token earnings
+     */
+    function deposit(address prizeToken, uint256[] memory ticketNumbers) public {
+        
+        // check the token a player is going to use is one of prize tokens
+        require(prizeTokensInstances[prizeToken] != MyAdvancedToken(0));
 
+        MyAdvancedToken prizeTokenContract = MyAdvancedToken(prizeToken);
+
+        // check the player has enough prize token
+        prizeTokenContract.balanceOf(msg.sender);
+        
+        
+        // save the bet
+        bets[msg.sender] = ticketNumbers;
     }
 
     function execute() public {
@@ -47,7 +69,19 @@ contract Raffle is Owned {
         name = paramName;
     }
     function setPrizeTokens(address[] memory paramPrizeTokens) onlyOwner public {
+        unsetPrizeTokensInstances();
         prizeTokens = paramPrizeTokens;
+        setPrizeTokensInstances();
+    }
+    function unsetPrizeTokensInstances() private {
+        for (uint256 i=0; i<prizeTokens.length; i++) {
+            prizeTokensInstances[prizeTokens[i]] = MyAdvancedToken(0);
+        }
+    }
+    function setPrizeTokensInstances() private {
+        for (uint256 i=0; i<prizeTokens.length; i++) {
+            prizeTokensInstances[prizeTokens[i]] = MyAdvancedToken(prizeTokens[i]);
+        }
     }
     function setDepositLimit(uint256 paramDepositLimit) onlyOwner public {
         depositLimit = paramDepositLimit;
