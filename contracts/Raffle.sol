07d118798b0fc747e0ab1c2f88bba2fab2e18122
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-06-25 20:38:48 +0300

    refactoring

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -17,44 +17,45 @@ contract Raffle is Owned {
     mapping (address => MyAdvancedToken) prizeTokensInstances;
 
     constructor(
-          string    memory paramName,
-          address[] memory paramPrizeTokens,
-          uint256          paramDepositLimit,
-          uint256          paramExecLimit,
-          uint32           paramExecTimestamp,
-          uint32           paramExecDelay,
-          string    memory paramSponsoredBy)
+          string    memory _name,
+          address[] memory _prizeTokens,
+          uint256          _depositLimit,
+          uint256          _execLimit,
+          uint32           _execTimestamp,
+          uint32           _execDelay,
+          string    memory _sponsoredBy)
       public {
 
-        setName(paramName);
-        setPrizeTokens(paramPrizeTokens);
-        setDepositLimit(paramDepositLimit);
-        setExecLimit(paramExecLimit);
-        setExecTimestamp(paramExecTimestamp);
-        setExecDelay(paramExecDelay);
-        setSponsoredBy(paramSponsoredBy);
+        setName(_name);
+        setPrizeTokens(_prizeTokens);
+        setDepositLimit(_depositLimit);
+        setExecLimit(_execLimit);
+        setExecTimestamp(_execTimestamp);
+        setExecDelay(_execDelay);
+        setSponsoredBy(_sponsoredBy);
     }
 
      /**
      * Called by players when they want to deposit tickets. The winner will be identified 
      * by the account belonging to the address of the sender depositing the tickets.
      *
-     * @param prizeToken the prize token which the player wants to deposit to make a bet
-     * @param ticketNumbers the ticket numbers on which the player deposits his prize token earnings
+     * @ _prizeToken the prize token which the player wants to deposit to make a bet
+     * @ _ticketNumbers the ticket numbers on which the player deposits his prize token earnings
      */
-    function deposit(address prizeToken, uint256[] memory ticketNumbers) public {
+    function deposit(address _prizeToken, uint256[] memory _ticketNumbers) public {
         
         // check the token a player is going to use is one of prize tokens
-        require(prizeTokensInstances[prizeToken] != MyAdvancedToken(0));
+        require(prizeTokensInstances[_prizeToken] != MyAdvancedToken(0));
 
-        MyAdvancedToken prizeTokenContract = MyAdvancedToken(prizeToken);
+        MyAdvancedToken prizeTokenContract = MyAdvancedToken(_prizeToken);
 
         // check the player has enough prize token
         prizeTokenContract.balanceOf(msg.sender);
         
+        // ...
         
         // save the bet
-        bets[msg.sender] = ticketNumbers;
+        bets[msg.sender] = _ticketNumbers;
     }
 
     function execute() public {
@@ -65,12 +66,12 @@ contract Raffle is Owned {
   
     }
 
-    function setName(string memory paramName) onlyOwner public {
-        name = paramName;
+    function setName(string memory _name) onlyOwner public {
+        name = _name;
     }
-    function setPrizeTokens(address[] memory paramPrizeTokens) onlyOwner public {
+    function setPrizeTokens(address[] memory _prizeTokens) onlyOwner public {
         unsetPrizeTokensInstances();
-        prizeTokens = paramPrizeTokens;
+        prizeTokens = _prizeTokens;
         setPrizeTokensInstances();
     }
     function unsetPrizeTokensInstances() private {
@@ -83,19 +84,19 @@ contract Raffle is Owned {
             prizeTokensInstances[prizeTokens[i]] = MyAdvancedToken(prizeTokens[i]);
         }
     }
-    function setDepositLimit(uint256 paramDepositLimit) onlyOwner public {
-        depositLimit = paramDepositLimit;
+    function setDepositLimit(uint256 _depositLimit) onlyOwner public {
+        depositLimit = _depositLimit;
     }
-    function setExecLimit(uint256 paramExecLimit) onlyOwner public {
-        execLimit = paramExecLimit;
+    function setExecLimit(uint256 _execLimit) onlyOwner public {
+        execLimit = _execLimit;
     }
-    function setExecTimestamp(uint32 paramExecTimestamp) onlyOwner public {
-        execTimestamp = paramExecTimestamp;
+    function setExecTimestamp(uint32 _execTimestamp) onlyOwner public {
+        execTimestamp = _execTimestamp;
     }
-    function setExecDelay(uint32 paramExecDelay) onlyOwner public {
-        execDelay = paramExecDelay;
+    function setExecDelay(uint32 _execDelay) onlyOwner public {
+        execDelay = _execDelay;
     }
-    function setSponsoredBy(string memory paramSponsoredBy) onlyOwner public {
-        sponsoredBy = paramSponsoredBy;
+    function setSponsoredBy(string memory _sponsoredBy) onlyOwner public {
+        sponsoredBy = _sponsoredBy;
     }
 }
