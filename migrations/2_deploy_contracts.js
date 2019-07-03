030138011779b0f8efdf104fbbcb26efc908f38f
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-07-03 19:02:32 +0300

    testing Ticket transferring

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ const AdvancedToken = artifacts.require("AdvancedToken")
 const ERC20 = artifacts.require("ERC20")
 
 const Web3 = require("web3")
-let web3
+let web3, accs
 
 function iterate(obj, stack) {
     for (var property in obj) {
@@ -39,21 +39,35 @@ module.exports = function(deployer) {
     
     web3 = new Web3(new Web3.providers.HttpProvider("http://" + network.host + ":" + network.port))
     getAccounts().then(function(accounts) {
-        getBalance(accounts[0]).then(function(balance) {
-            console.log("Promised: " + balance)
+        accs = accounts
+        if (accs[0] != network.from) throw "Something is wrong with the network or web3"
+
+        getBalance(accs[0]).then(function(balance) {
+            console.log("#### accs[0] ether balance: " + balance)
         })
+
     })
 
     deployer.deploy(Ticket, "Ticket", "TICKET").then(function(instance) {
-        instance.mintAmount(network.from, 25).then(function() {
-            instance.balanceOf(network.from).then(function(balance) {
-                console.log("Promised Ticket Balance: " + balance)
+        instance.mintAmount(accs[0], 25).then(function() {
+            instance.balanceOf(accs[0]).then(function(balance) {
+                console.log("#### accs[0] ticket balance before transferring: " + balance + "####")
+                instance.safeTransferFrom(accs[0], accs[5], 22).then(function() {
+                    instance.balanceOf(accs[0]).then(function(balance) {
+                        console.log("#### accs[0] ticket balance after transferring: " + balance + "####")
+                        instance.ownerOf(22).then(function(owner) {
+                            console.log("#### the owner of ticket #22: " + owner + "####")
+                        })
+                    })
+                })
             })
         })
     })
+
     deployer.deploy(ERC721Full, "ERC-721 Standard", "ERC721");
     deployer.deploy(AdvancedToken, 1000, "Advanced Token", "ADV");
     deployer.deploy(ERC20, 1000000, "ERC-20 Standard", "ERC20");
+
 }
 
 
