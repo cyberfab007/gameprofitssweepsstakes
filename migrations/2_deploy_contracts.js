b8d2a99acda6bf9dfc38bcdd946a230f6b5cf54a
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-07-05 13:40:53 +0300

    Raffle deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,23 @@ const Ticket = artifacts.require("Ticket")
 const ERC721Full = artifacts.require("ERC721Full")
 const AdvancedToken = artifacts.require("AdvancedToken")
 const ERC20 = artifacts.require("ERC20")
+const Raffle = artifacts.require("Raffle")
 
 module.exports = function(deployer) {
-    deployer.deploy(Ticket, "Ticket", "TICKET")
-    deployer.deploy(ERC721Full, "ERC-721 Standard", "ERC721")
-    deployer.deploy(AdvancedToken, 1000, "Advanced Token", "ADV")
-    deployer.deploy(ERC20, 1000000, "ERC-20 Standard", "ERC20")
+    let ticketToken, prizeTokens = []
+    deployer.deploy(Ticket, "Ticket", "TICKET").then(instance => {
+        ticketToken = instance.address
+        return deployer.deploy(ERC721Full, "ERC-721 Standard", "ERC721").then(instance => {
+            prizeTokens.push(instance.address)
+            return deployer.deploy(AdvancedToken, 1000, "Advanced Token", "ADV").then(instance => {
+                prizeTokens.push(instance.address)
+                return deployer.deploy(ERC20, 1000000, "ERC-20 Standard", "ERC20").then(instance => {
+                    prizeTokens.push(instance.address)
+                    return deployer.deploy(Raffle, "My First Raffle", ticketToken, prizeTokens, true, 101, 102, 103, 104, "Our Good Sponsor")
+                })
+            })
+        })
+    })
 }
 
 /*
