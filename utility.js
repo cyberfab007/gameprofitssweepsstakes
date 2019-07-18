bdd5679976074fcf6f47b88343300fb7367d5753
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-07-18 16:55:19 +0300

    fixed utility.js

--- utility.js
+++ utility.js
@@ -3,23 +3,26 @@
 
 var totalAmount = 1000;
 var mintedAmount = 0;
+var interval;
 
-function mintBatchERC721(receiver, token, amount) {
+var batchMintERC721 = function () {
 
     var params = {
-      "from": receiver ? receiver : "0x2e16f253e0a3b544f7e755a8d904976adaea7833",
-      "to": token ? token : "0xD32F8de3d5DAB3A61c3c58046E086065FEC4168c",
+      "from": "0x2e16f253e0a3b544f7e755a8d904976adaea7833",
+      "to": "0xD32F8de3d5DAB3A61c3c58046E086065FEC4168c",
       "gas": "0x7A1200",
       "gasPrice": "0x37E11D600",
       "data": "0x0e583dd20000000000000000000000002e16f253e0a3b544f7e755a8d904976adaea78330000000000000000000000000000000000000000000000000000000000000032"
     };
-
+    console.log(params)
     eth.sendTransaction(params);
-
+ 
     mintedAmount += 50;
     if (mintedAmount > totalAmount) {
         clearInterval(interval);
     }
 }
 
-var interval = setInterval(mintBatchERC721(null,null,null), 15000);
+var start = function () {
+    interval = setInterval(batchMintERC721, 15000);
+}
