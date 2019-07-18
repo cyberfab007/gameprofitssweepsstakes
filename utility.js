d767f28a8141010eec8d9abc064f24aec5492020
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-07-18 16:18:04 +0300

    added utility.js

---
+++ utility.js
@@ -0,0 +1,25 @@
+
+// 20 transactions * 8000000 gas * 15000000000 wei = 240*10^1+6+9 = 2.4 ETH
+
+const totalAmount = 1000
+let mintedAmount = 0
+
+function mintBatchERC721(receiver, token, amount) {
+
+    const params = {
+      "from": receiver ? receiver : "0x2e16f253e0a3b544f7e755a8d904976adaea7833",
+      "to": token ? token : "0xD32F8de3d5DAB3A61c3c58046E086065FEC4168c",
+      "gas": "0x7A1200",
+      "gasPrice": "0x37E11D600",
+      "data": "0x0e583dd20000000000000000000000002e16f253e0a3b544f7e755a8d904976adaea78330000000000000000000000000000000000000000000000000000000000000032"
+    }
+
+    eth.sendTransaction(params)
+
+    mintedAmount += 50
+    if (mintedAmount > totalAmount) {
+        clearInterval(interval);
+    }
+}
+
+var interval = setInterval(mintBatchERC721(null,null,null), 15000)
