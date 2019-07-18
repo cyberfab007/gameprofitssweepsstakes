8622aa53bb408de858d8b7ec59f55cfe28c2a381
Aliaksandr Adzinets <aliaks.adzinets@gmail.com>
2019-07-18 16:57:54 +0300

    logging in utility.js

--- utility.js
+++ utility.js
@@ -7,6 +7,8 @@ var interval;
 
 var batchMintERC721 = function () {
 
+    console.log("Tickets left to mint: " + (totalAmount - mintedAmount));
+
     var params = {
       "from": "0x2e16f253e0a3b544f7e755a8d904976adaea7833",
       "to": "0xD32F8de3d5DAB3A61c3c58046E086065FEC4168c",
@@ -14,7 +16,7 @@ var batchMintERC721 = function () {
       "gasPrice": "0x37E11D600",
       "data": "0x0e583dd20000000000000000000000002e16f253e0a3b544f7e755a8d904976adaea78330000000000000000000000000000000000000000000000000000000000000032"
     };
-    console.log(params)
+
     eth.sendTransaction(params);
  
     mintedAmount += 50;
